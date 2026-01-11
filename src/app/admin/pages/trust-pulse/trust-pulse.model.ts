1e136e3eaed855e3a381cd8bbb4fe3719caee549
vuxwindy <congiotruockhibao@gmail.com>
2026-01-11 15:22:14 +0700

    updated landing page content and images

--- src/app/admin/pages/trust-pulse/trust-pulse.model.ts
+++ src/app/admin/pages/trust-pulse/trust-pulse.model.ts
@@ -2,6 +2,7 @@ export type TrustStatus = 'running' | 'completed' | 'stopped';
 
 export interface TrustPulseItem {
   id: number;
+  userName?: string;
   pair: string;
   exchange: string;
   value: number;
